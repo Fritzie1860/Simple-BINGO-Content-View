823ea9ebb19654c427acf5846391ffcc8d26f53d
Fritzie Primananda Adi Praja <primanandafritzie@gmail.com>
2022-03-28 10:12:01 +0800

    Initial Commit

---
+++ BINGO/BINGOApp.swift
@@ -0,0 +1,17 @@
+//
+//  BINGOApp.swift
+//  BINGO
+//
+//  Created by Fritzie Primananda Adi Praja on 28/03/22.
+//
+
+import SwiftUI
+
+@main
+struct BINGOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
